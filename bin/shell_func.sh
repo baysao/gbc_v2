f571453b59dae24606fd4c256a866791bbf32be2
Vu Tran <baysao@gmail.com>
2022-10-12 15:43:29 +0700

    add common check in shell_func

--- bin/shell_func.sh
+++ bin/shell_func.sh
@@ -10,12 +10,29 @@ fi
 
 #cd $ROOT_DIR
 
-LUA_BIN=/usr/local/openresty/luajit/bin/luajit
+LUA_BIN=$ROOT_DIR/bin/openresty/luajit/bin/luajit
 
 TMP_DIR=$ROOT_DIR/tmp
 CONF_DIR=$ROOT_DIR/gbc/conf
 CONF_PATH=$CONF_DIR/config.lua
 VAR_SUPERVISORD_CONF_PATH=$TMP_DIR/supervisord.conf
+
+if [ -e "$ROOT_DIR/bin/openresty" ]; then
+	cp -rf $ROOT_DIR/gbc/openresty/luajit/share/lua/5.1/* $ROOT_DIR/bin/openresty/luajit/share/lua/5.1/
+	cp -rf $ROOT_DIR/gbc/openresty/luajit/lib/lua/5.1/* $ROOT_DIR/bin/openresty/luajit/lib/lua/5.1/
+	echo $ROOT_DIR/bin/openresty/v1.21.4.1/lib >/etc/ld.so.conf.d/0openresty.conf
+	ldconfig
+	export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$ROOT_DIR/bin/openresty/bin:$ROOT_DIR/bin/openresty/luajit/bin:$ROOT_DIR/bin/openresty/nginx/sbin
+else
+	echo "Missing $ROOT_DIR/bin/openresty"
+	exit 1
+
+fi
+
+if [ ! -f "/usr/bin/python" ]; then
+	apt update
+	apt install -y python-is-python2
+fi
 
 # function getOsType()
 # {
@@ -166,9 +183,9 @@ function runTests() {
 function updateConfigs() {
 	# if [ ! -f "/tmp/updateconfig.lock" ]; then
 	# 	touch /tmp/updateconfig.lock
-		# echo "ROOT_DIR:$ROOT_DIR"
-		if [ -z "$BIND_ADDRESS" ]; then BIND_ADDRESS="0.0.0.0"; fi
-		$LUA_BIN -e "BIND_ADDRESS=\"$BIND_ADDRESS\";ROOT_DIR='$ROOT_DIR'; DEBUG=$DEBUG; dofile('$ROOT_DIR/gbc/bin/shell_func.lua'); updateConfigs()"
+	# echo "ROOT_DIR:$ROOT_DIR"
+	if [ -z "$BIND_ADDRESS" ]; then BIND_ADDRESS="0.0.0.0"; fi
+	$LUA_BIN -e "BIND_ADDRESS=\"$BIND_ADDRESS\";ROOT_DIR='$ROOT_DIR'; DEBUG=$DEBUG; dofile('$ROOT_DIR/gbc/bin/shell_func.lua'); updateConfigs()"
 	# 	rm /tmp/updateconfig.lock
 	# fi
 }
